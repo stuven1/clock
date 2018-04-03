f92f4e1b3df9c81093e25a6586ab871027ce5819
stuven1 <moaheterjag@icloud.com>
2018-04-03 15:06:55 +0200

    assignment-clock Moa

--- code/src/index.js
+++ code/src/index.js
@@ -3,16 +3,59 @@ import ReactDOM from "react-dom"
 import "./index.css"
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { date: new Date() }
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  pause = () => {
+    clearInterval(this.timerID)
+  }
+
+  restart = () => {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  clockName = () => {
+    this.state.date.getMinutes()
+  }
 
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <div className={this.clockName()}>
+
+          <img src="./time.png" alt="Im a clock" width="150px" height="150px" />
+          <h1>Hello world!</h1>
+          <h4>The current time is:</h4>
+          <p>{this.state.date.toLocaleTimeString()}</p>
+
+          <button className="button-1" onClick={this.pause}>Stop time</button>
+          <button className="button-2" onClick={this.restart}>Restart time</button>
+        </div>
       </div>
     )
   }
-
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+ReactDOM.render(<Clock />, document.getElementById("root"))
